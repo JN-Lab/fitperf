80f0be40f58cf77eee60a31096fdd9eb7f4cb841
juliennuellas <julien.nuellas@gmail.com>
2018-09-17 10:29:41 +0200

    Set up Travis

---
+++ fitperf/fitperf/settings/travis.py
@@ -0,0 +1,12 @@
+from . import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
