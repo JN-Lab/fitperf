7b644e1b2a6d8d8bb7a2d9b627b438ab42d85f9f
juliennuellas <julien.nuellas@gmail.com>
2018-09-18 14:45:01 +0200

    deeply changed the models structure

--- fitperf/program_builder/models.py
+++ fitperf/program_builder/models.py
@@ -9,20 +9,44 @@ class Profile(models.Model):
     and exercises.
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    programs = models.ManyToManyField('Program',
-                                       through='Session',
-                                       related_name='profiles',
-                                       verbose_name="list of programs per user")
-    trainings = models.ManyToManyField('Training',
-                                        through='Session',
-                                        related_name='profiles',
-                                        verbose_name="list of trainings per user")
-    exercises = models.ManyToManyField('Exercise',
-                                        through='Session',
-                                        verbose_name="list of exercises per user")
  
     def __str__(self):
         return self.user.username
+
+class Session(models.Model):
+    user = models.ForeignKey('Profile',
+                             on_delete=models.CASCADE,
+                             verbose_name="user who does the session")
+    program = models.ForeignKey('Program', 
+                                on_delete=models.CASCADE,
+                                null=True,
+                                verbose_name="program linked to the session if exists")
+    training = models.ForeignKey('Training',
+                                on_delete=models.CASCADE,
+                                verbose_name="training associated to the session")
+    date = models.DateTimeField(default=timezone.now)
+    done = models.BooleanField(default=False)
+
+    exercises = models.ManyToManyField('Exercise',
+                                        through='ExercisesPerSession',
+                                        related_name='sessions',
+                                        verbose_name="list of exercises per session")
+    
+    def __str__(self):
+        return self.training + "-"  + self.date
+
+class ExercisesPerSession(models.Model):
+    exercise = models.ForeignKey('Exercise',
+                                 on_delete=models.CASCADE)
+    session = models.ForeignKey('Session',
+                                 on_delete=models.CASCADE)
+    challenge = models.BooleanField(default=False)
+    performance = models.DecimalField(max_digits=5, 
+                                     decimal_places=1,
+                                     null=True)
+
+    def __str__(self):
+        return self.session + "-" + self.exercise
 
 class Program(models.Model):
     """
@@ -36,14 +60,8 @@ class Program(models.Model):
                                 on_delete=models.CASCADE,
                                  verbose_name="the program's creator")
     trainings = models.ManyToManyField('Training',
-                                        through='Session',
                                         related_name='programs',
                                         verbose_name='list of trainings per program')
-    exercises = models.ManyToManyField('Exercise',
-                                        through='Session',
-                                        related_name='programs',
-                                        verbose_name='list of exercises per program')
-
     def __str__(self):
         return self.name
 
@@ -56,43 +74,8 @@ class Training(models.Model):
                                 on_delete=models.CASCADE, 
                                 verbose_name="the training's creator")
     exercises = models.ManyToManyField('Exercise',
-                                        through='Session',
                                         related_name='trainings',
                                         verbose_name='list of exercises per training')
-
-    def __str__(self):
-        return self.name
-
-class MovementSettings(models.Model):
-    """
-    This class represents the different settings a movement can be
-    associated with.
-    """
-    MOVEMENTS_SETTINGS = (
-        ('REP', 'Repetitions'),
-        ('WEIGTH', 'Poids'),
-        ('DISTANCE', 'Distance'),
-    )
-    name = models.CharField(max_length=20,
-                            choices=MOVEMENTS_SETTINGS)
-    unity = models.CharField(max_length=5)
-
-    def __str__(self):
-        return self.name
-
-class Movement(models.Model):
-    """
-    This class represents the movements created
-    """
-    name = models.CharField(max_length=50)
-    equipment = models.CharField(max_length=20,
-                                 null=True)
-    founder = models.ForeignKey('Profile',
-                                on_delete=models.CASCADE,
-                                verbose_name="the movement's creator")
-    settings = models.ManyToManyField('MovementSettings',
-                                     related_name='movements',
-                                     verbose_name="list of settings")
 
     def __str__(self):
         return self.name
@@ -151,23 +134,36 @@ class MovementsPerExercise(models.Model):
     def __str__(self):
         return self.exercise + " / " + self.movement
 
-class Session(models.Model):
+class Movement(models.Model):
     """
-    This class linked all the different elements to create a unique activity.
-    The idea is to get an exercise which is linked to a training, also linked
-    to a program and which is realized by a specific user.
+    This class represents the movements created
     """
-    user = models.ForeignKey('Profile',
-                             on_delete=models.CASCADE)
-    program = models.ForeignKey('Program',
-                                on_delete=models.CASCADE)
-    training = models.ForeignKey('Training',
-                                 on_delete=models.CASCADE)
-    exercise = models.ForeignKey('Exercise',
-                                on_delete=models.CASCADE)
-    date = models.DateTimeField()
-    challenge = models.BooleanField(default=False)
-    done = models.BooleanField(default=False)
-    performance = models.DecimalField(max_digits=5, 
-                                     decimal_places=1,
-                                     null=True)
+    name = models.CharField(max_length=50)
+    equipment = models.CharField(max_length=20,
+                                 null=True)
+    founder = models.ForeignKey('Profile',
+                                on_delete=models.CASCADE,
+                                verbose_name="the movement's creator")
+    settings = models.ManyToManyField('MovementSettings',
+                                     related_name='movements',
+                                     verbose_name="list of settings")
+
+    def __str__(self):
+        return self.name
+
+class MovementSettings(models.Model):
+    """
+    This class represents the different settings a movement can be
+    associated with.
+    """
+    MOVEMENTS_SETTINGS = (
+        ('REP', 'Repetitions'),
+        ('WEIGTH', 'Poids'),
+        ('DISTANCE', 'Distance'),
+    )
+    name = models.CharField(max_length=20,
+                            choices=MOVEMENTS_SETTINGS)
+    unity = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.name
---
+++ fitperf/program_builder/utils/db_interactions.py
@@ -0,0 +1,136 @@
+#! /usr/bin/env python3
+# coding: utf-8
+from django.contrib.auth.models import User
+from ..models import Profile, Program, Training, MovementSettings, Movement, Exercise, MovementsPerExercise, Session
+
+class DBSet:
+    """
+    This class manages all the interactions to register data
+    in the database
+    """
+    
+
+    # Enregistrer un profile (plutôt dans authentification -> pas sûr que ca soit là)
+
+    # Créer une caractéristique de mouvement
+
+    # Créer un mouvement (<-- Admin)
+        # Créer une association entre une caractéristique et un mouvement
+    
+    # Créer un exercice
+        # Créer une association entre un mouvement et un exercice
+    
+    # Créer un entraînement
+        # Créer une association entre un entraînement et un exercice
+    
+    # Créer un programme
+        # Créer une association entre un programme et un entraînement
+
+    # Créer une session
+        # Ca consiste à plusieurs choses:
+        #     - Associer un entrainements d un programme à une date
+        #     - Définir si les exercices de cet entrainement sont un challenge
+
+    def set_profile(self):
+        pass
+
+    def set_program(self):
+        pass
+
+class DBGet:
+    """
+    This class manages all the interactions to get data
+    in the database
+    """
+
+    # Récupérer tous les users
+        # Recupérer les performances d'un user !!! --> A construire dans un second temps
+    
+    # Récupérer toutes les sessions
+        # Récupérer une session
+        # Récupérer toutes les sessions réalisées par un user
+        # Récupérer toutes les sessions d'un programme
+        # Récupérer toutes les sessions terminées d'un user
+        # Récupérer toutes les sessions terminées d'un programme pour un user donné
+        # Récupérer toutes les sessions non terminées d'un user
+        # Récupérer toutes les sessions non terminées d'un programme pour un user donné
+        # Récupérer toutes les sessions de la journée en cours
+        # Récupérer toutes les sessions de la semaine en cours
+    
+    # Récupérer tous les programmes
+        # Récupérer un programme
+        # Récupérer tous les programmes créés par un user
+        # Récupérer tous les programmes créés par un user + les programmes créés par les admins
+    
+    # Récupérer tous les entraînements
+        # Récupérer un entraînement
+        # Récupérer tous les entraînements créés par un user
+        # Récupérer tous les entraînements créés par un user + les programmes créés par les admins
+        # Récupérer tous les entraînements associés à un programme
+
+    # Récupérer tous les exercices
+        # Récupérer un exercice
+        # Récupérer tous les exercices créés par un user
+        # Récupérer tous les exercices créés par un user + les exercices créés par les admins
+        # Récupérer tous les exercices associés à un programme
+        # Récupérer tous les exercices associés à un entraînement
+
+    # Récupérer tous les mouvements
+        # Récupérer un mouvement
+        # Récupérer tous les mouvements associés à un exercice
+
+    # Récupérer toutes les caractéristiques de mouvement
+        # Récupérer une caractéristique
+        # Récupérer toutes les caractéristique d'un mouvement
+        # Récupérer une caractéristique d'un mouvement
+
+    pass
+
+class DBAlter:
+    """
+    This class manages the interactions to modify data
+    in the database
+    """
+
+    # Modifier un user
+        # Modifier un mot de passe
+    
+    # Modifier un programme
+        # Modifier le nom d'un programme
+        # Modifier la date de départ d'un programme
+        # Modifier la date de fin / durée d'un programme
+        # Modifier la description d'un programme
+
+    # Modifier un entraînement
+        # Modifier le nom d'un entraînement
+
+    # Modifier un exercice
+        # Modifier le nom d'un exercice
+
+    # Modifier une session
+        # Modifier la date d'une session
+        # Modifier le statut done d'une session
+        # Modifier/ Enregistrer la performance sur un exercice d'une session
+        # Modifier le statut challenge d'un exercice d'une session
+
+    pass
+
+class DBDelete:
+    """
+    This class manages the interactions to delete data
+    in the database
+    """
+
+    # Supprimer un user
+    # Supprimer un programme
+    # Supprimer un entraînements
+    # Supprimer un entraînement d'un programme
+    # Supprimer un exercice
+    # Supprimer un exercice d'un entraînement
+    # Supprimer un mouvement
+    # Supprimer un mouvement d'un exercice
+    # Supprimer une caracteristique de mouvement
+    # Supprimer une caracteristique de mouvement d'un mouvement
+    # Supprimer une session
+
+    pass
