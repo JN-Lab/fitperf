8a82f2ea45cf67d484fc88dc19877e8ae6f38396
juliennuellas <julien.nuellas@gmail.com>
2018-09-26 16:20:28 +0200

    start js treatments

--- fitperf/static/javascript/custom.js
+++ fitperf/static/javascript/custom.js
@@ -2,18 +2,35 @@
 // Ajax Interactions
 // ----------------------------------
 
+// Ajax Get function
+function ajaxGet(url, callback) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            // appelle la fonction callback en lui passant la response de la requête
+            callback(req.responseText);
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    req.send(null);
+}
+
 // Ajax Post function
 function ajaxPost(url, data, callback, isJson) {
     var req = new XMLHttpRequest();
     req.open("POST", url);
-
-    req.addEventListener("load",function () {
+    req.addEventListener("load", function () {
         if (req.status >= 200 && req.status < 400) {
             // Appelle la fonction callback en lui passant la réponse de la requête
             callback(req.responseText);
         } else {
             console.error(req.status + " " + req.statusText + " " + url);
-        }       
+        }
     });
     req.addEventListener("error", function () {
         console.error("Erreur réseau avec l'URL " + url);
