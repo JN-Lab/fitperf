4f86b0d1ea990c5c26ecce0f6d721d3b323b059c
juliennuellas <julien.nuellas@gmail.com>
2018-09-28 18:58:53 +0200

    add unit tests on ajax_all_movements view

--- fitperf/program_builder/tests/test_views.py
+++ fitperf/program_builder/tests/test_views.py
@@ -70,3 +70,24 @@ class MovementsListPageTestCase(TestCase):
     def test_movements_list_page_page_when_not_logged(self):
         response = self.client.get(reverse('program_builder:movements_list'))
         self.assertEqual(response.status_code, 302)
+
+class AjaxAllMovementsPageTestCase(TestCase):
+    """
+    This class tests the ajax_all_movements view
+    """
+    
+    @classmethod
+    def setUpTestData(cls):
+        username = 'user'
+        user = User.objects.create_user(username=username)
+        user.set_password('test-view')
+        user.save()
+
+    def test_movements_list_page_page_when_logged(self):
+        self.client.login(username='user', password='test-view')      
+        response = self.client.get(reverse('program_builder:ajax_all_movements'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_movements_list_page_page_when_not_logged(self):
+        response = self.client.get(reverse('program_builder:ajax_all_movements'))
+        self.assertEqual(response.status_code, 302)
