6dc82d4e047c2a1e3b825fc0266d380580bd5e81
juliennuellas <julien.nuellas@gmail.com>
2018-10-16 19:38:09 +0200

    worked on pb print for exercises-list

--- fitperf/program_builder/views.py
+++ fitperf/program_builder/views.py
@@ -48,17 +48,18 @@ def exercises_list(request):
         - print a list of exercise
         - create an exercise and redirect on the exercise page to finalize
     """
+    tools = Tools()
     db = DataTreatment()
     exercises = db.get_all_exercises_dict_linked_to_one_user(request.user)
-    exercises_number = len(exercises)
-    custom_exercises_number = len([exercise for exercise in exercises if not exercise["is_default"]])
-    pb_number = len([exercise for exercise in exercises if exercise["pb"]])
+    for exercise in exercises:
+        if exercise["pb"] != 0 and exercise["goal_type"] == "duree":
+            exercise["pb"] = tools.convert_seconds_into_time(exercise["pb"])
     new_exercise_form = RegisterExerciseStep1()
     context = {
         'exercises': exercises,
-        'exercises_number': exercises_number,
-        'custom_exercises_number': custom_exercises_number,
-        'pb_number': pb_number,
+        'exercises_number': len(exercises),
+        'custom_exercises_number': len([exercise for exercise in exercises if not exercise["is_default"]]),
+        'pb_number': len([exercise for exercise in exercises if exercise["pb"] != 0]),
         'new_exercise_form': new_exercise_form,
     }
     return render(request, 'exercises_list.html', context)
