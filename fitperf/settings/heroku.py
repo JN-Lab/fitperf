ee017daa5bd4f8171929a341eb286fe5c42ea87d
juliennuellas <julien.nuellas@gmail.com>
2018-10-22 20:02:49 +0200

    try to resolve static delivery problem

--- fitperf/settings/heroku.py
+++ fitperf/settings/heroku.py
@@ -13,8 +13,6 @@ ALLOWED_HOSTS = ['fitperf.herokuapp.com']
 
 # For static management during production
 
-MIDDLEWARE = MIDDLEWARE + ['whitenoise.middleware.WhiteNoiseMiddleware']
-
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_DIRS = (
